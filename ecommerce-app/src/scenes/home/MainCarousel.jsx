519733d6e2c933373f8c666a66fe8f961bdd904a
Climson Jr <jrbuking@gmail.com>
2023-03-28 23:41:32 +1100

    small changes

--- ecommerce-app/src/scenes/home/MainCarousel.jsx
+++ ecommerce-app/src/scenes/home/MainCarousel.jsx
@@ -2,4 +2,5 @@ import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
 import { Carousel } from "react-responsive-carousel"; //Allows the hero section to have a carousel
 export const MainCarousel = () => {
   return;
+  <div>Hello world</div>;
 };
